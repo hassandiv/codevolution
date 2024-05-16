1626b7c5a88e5e79c8138a0863073d6fa5346a5e
hassan hafez <hassanhafez86@gmail.com>
2024-05-16 23:45:30 +0300

    prime number

--- isPrime.js
+++ isPrime.js
@@ -22,11 +22,11 @@ console.log("isPrime", isPrime(23)); //true
 //no other number than 1 should give out the same result as the number "n"
 //example n = 15, answer is 1*15 = 15, 15*1 = 15, because 15 accepts multiply by 3, 15*3 = 15 this is not a prime number
 
-//n % i this means the difference/remainder after the division or multiply to reach the given number "n"
+//n % i this means the difference/remainder after the division to reach the given number "n"
 // we are dividing a number "n" on "i", "i" is changing due to the loop, "n" doesn't change, so we have many probabilities
 //example n = 30, then 30 % 2, 30 % 3, 30 % 4, 30 % 5, ...etc until it reach 30 % 29
 //if any probabilities of the 30 muliplied by all of the "i" has "remainders %" and is equal to === 0 then it will not be a prime number
-//example 4 * 7 = 28 close to 30, our remainder is 2s
+//example 4 * 7 = 28 close to 30, our remainder is 2
 
 const isPrimeOptimised = (n) => {
 	if (n < 2) {
