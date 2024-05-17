56afdf35e5bdf31d31e1c270f9304f1ddf8929c3
hassan hafez <hassanhafez86@gmail.com>
2024-05-17 17:25:17 +0300

    update on fib sq + rec fib seq

--- RecusiveFibonacciSeq.js
+++ RecusiveFibonacciSeq.js
@@ -23,3 +23,18 @@ console.log("recursiveFibonacci", recursiveFibonacci(6)); //8
 // recursiveFibonacci(2) = 1  + recursiveFibonacci(1) = 1  + recursiveFibonacci(1) = 1  + recursiveFibonacci(0) = 0  + recursiveFibonacci(1) = 1  + recursiveFibonacci(0) = 0  + recursiveFibonacci(1) = 1  + recursiveFibonacci(0) = 0
 //  /                      \
 // recursiveFibonacci(1) = 1  + recursiveFibonacci(0) = 0
+
+// O(n) iterative for exampel for loop. normal fib seq
+// O(2^n) - Recursion - "O off (2 power n)" time complexity
+
+// To find the nth number of the Fibonacci sequence, you can use the following formula:
+// Fn = Fn-1 + Fn-2
+// where F1 = 1 and F2 = 1 are the first two numbers in the sequence.
+// For the 6th number in the Fibonacci sequence, we can use the formula to calculate it step by step:
+
+// F1 = 1
+// F2 = 1
+// F3 = F2 + F1 = 1 + 1 = 2
+// F4 = F3 + F2 = 2 + 1 = 3
+// F5 = F4 + F3 = 3 + 2 = 5
+// F6 = F5 + F4 = 5 + 3 = 8
